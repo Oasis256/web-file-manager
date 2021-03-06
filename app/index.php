708397157759f404b5d3c3c10b63644d838e437a
Oasis Innocent <oasis.sybill@webshule.com>
2021-03-06 07:59:12 +0300

    File Manager in Docker

--- app/index.php
+++ app/index.php
@@ -1,39 +1,7 @@
 <?php
-
-$magicNumbers = [
-    rand(0, 100),
-    rand(100, 200),
-    rand(0, 1000),
-];
-
+	ob_start();
+	include ('config/config.php');
+	$app = new Application();
+	$app->setDefault('user.index.index');
+	$app->run();
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta name="author" content="Steven Liebregt" />
-        <title>Hello, world!</title>
-        <style>
-            table, th, td {
-                border: 1px solid black;
-                border-collapse: collapse;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Hello, world!</h1>
-        <p>Your magic numbers are:</p>
-        <table>
-            <tr>
-                <th>#</th>
-                <th>Number</th>
-            <tr>
-            <?php foreach ($magicNumbers as $index => $number): ?>
-                <tr>
-                    <td><?php echo $index; ?></td>
-                    <td><?php echo $number; ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </table>
-        <p>If you can see this properly, then it means PHP is working fine.</p>
-    </body>
-</html>
