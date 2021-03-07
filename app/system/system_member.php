f3ae83f854d22910cd717f3cdc1f01ab6329e3a4
Ubuntu <Oasis@Relay.wrfpmu5imuhebdnsicsj302cng.cwx.internal.cloudapp.net>
2021-03-07 00:11:31 +0000

    Here We Go

---
+++ app/system/system_member.php
@@ -0,0 +1,71 @@
+<?php exit;?>{
+    "1": {
+        "name": "admin",
+        "path": "admin",
+        "password": "714d82a0a84f9c6e3495fe2aa5618627",
+        "userID": "1",
+        "role": "1",
+        "config": {
+            "sizeMax": "0",
+            "sizeUse": 1024
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "createTime": 1614987763,
+        "status": 1,
+        "lastLogin": 1614987922
+    },
+    "100": {
+        "userID": "100",
+        "name": "demo",
+        "password": "fe01ce2a7fbac8fafaed7c982a04e229",
+        "role": "2",
+        "config": {
+            "sizeMax": 5,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "demo",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1614987763
+    },
+    "101": {
+        "userID": "101",
+        "name": "guest",
+        "password": "084e0343a0486ff05530df6c705c8bb4",
+        "role": "100",
+        "config": {
+            "sizeMax": 0.1,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "read"
+        },
+        "path": "guest",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1614987763
+    },
+    "102": {
+        "userID": "102",
+        "name": "Oasis",
+        "nickName": "Oasis",
+        "password": "714d82a0a84f9c6e3495fe2aa5618627",
+        "role": "1",
+        "config": {
+            "sizeMax": 2,
+            "sizeUse": 1048576
+        },
+        "groupInfo": {
+            "1": "write"
+        },
+        "path": "Oasis",
+        "status": 1,
+        "lastLogin": "",
+        "createTime": 1614989361
+    }
+}
